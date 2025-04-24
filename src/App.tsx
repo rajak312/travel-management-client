c525f1f48499cd588faa04f96c7535a4c13fd7bf
Lalit Rajak <lalitrajak312@gmail.com>
2025-04-24 21:03:04 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center text-3xl font-semibold text-indigo-600 py-10">
+      🚀 Travel Booking App (Frontend with TypeScript + Tailwind)
+    </div>
+  );
+}
+
+export default App;
